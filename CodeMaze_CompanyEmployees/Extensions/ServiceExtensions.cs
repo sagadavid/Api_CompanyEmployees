019646d8d52e4507437caa9ec0d8f0541c314412
sagadavid <epostsaga@gmail.com>
2023-02-03 10:19:30 +0100

    Add project files.

---
+++ CodeMaze_CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CodeMaze_CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+            => services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+                });
+        
+    }
+}
