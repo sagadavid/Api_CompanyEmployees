80d24eb86df111096d27c9f44cdb6eb6a9686736
sagadavid <epostsaga@gmail.com>
2023-02-08 00:24:37 +0100

    CONTEXTFACTORY, REPOSITORYCONTEXT, ENTITY MODELS, SQL CONNECTION, DBSET

---
+++ CodeMaze_CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace CodeMaze_CompanyEmployees.ContextFactory
+{
+    //Since our RepositoryContext class is in a Repository project
+    //and not in the main one, this class will help our application
+    //create a derived DbContext instance during the design time which
+    //will help us with our migrations:
+    public class RepositoryContextFactory : 
+        IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        //implementtion of idesigntimedbcontextfactory
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+
+.SetBasePath(Directory.GetCurrentDirectory())
+
+.AddJsonFile("appsettings.json")
+
+.Build();
+
+            var builder = new DbContextOptionsBuilder<RepositoryContext>()
+
+            .UseSqlServer(configuration.GetConnectionString("sqlConnection"));
+
+            return new RepositoryContext(builder.Options);
+        }
+    }
+}
