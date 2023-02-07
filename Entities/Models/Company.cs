f3d0f7a4a837d00dbe7658451a2d5831f5e09d19
sagadavid <epostsaga@gmail.com>
2023-02-07 21:40:35 +0100

    ONION ARCHITECTURE, CLASS LIBRARY PROJECT-ENTITIES,

---
+++ Entities/Models/Company.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Company
+    {
+        [Column("CompanyId")]//will be mapped to DB as shown
+        public Guid Id { get; set; }
+        
+        [Required(ErrorMessage ="name is required")]
+        [MaxLength(60, ErrorMessage ="up to 60 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "address is required ")]
+        [MaxLength(60, ErrorMessage = "up to 60 characters")]
+        public string? Address { get; set; }
+
+        public string? Country { get; set; }
+
+        public ICollection<Employee>? Employees { get; set; }//navigational property
+                                                            
+    }
+}
---
+++ Entities/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+   public class Employee
+    {
+        [Column("EmployeeId")]//will be mapped to DB as shown
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "name is required")]
+        [MaxLength(60, ErrorMessage = "up to 30 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="age is required")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "position is required ")]
+        [MaxLength(60, ErrorMessage = "up to 20 characters")]
+        public string? Position { get; set; }
+
+        [ForeignKey(nameof(Company))]//A nameof expression produces
+                                     //the name of a variable, type, or member
+                                     //as the string constant.
+        public Guid CompanyId { get; set; }//navigational property
+        public Company? Company { get; set; }////navigational property
+    }
+}
