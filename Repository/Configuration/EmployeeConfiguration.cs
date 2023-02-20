10f049291a1efbb5f1dc57e4f859a76abc0b9b20
sagadavid <epostsaga@gmail.com>
2023-02-20 13:43:06 +0100

    REPOSITORY PATTERN LOGIC, ADDED

--- Repository/Configuration/EmployeeConfiguration.cs
+++ Repository/Configuration/EmployeeConfiguration.cs
@@ -1,12 +1,6 @@
 ﻿using Entities.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Repository;
 
 namespace Repository.Configuration
 {
---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,46 @@
+﻿using Contracts;//important to add
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected RepositoryContext 
+            _repositoryContext;
+
+        public RepositoryBase(RepositoryContext repositoryContext)
+            //=>
+        {_repositoryContext = repositoryContext;}
+        //RepositoryContext=repositoryContext;
+
+        public IQueryable<T> FindAll(bool trackChanges) =>
+        !trackChanges ?
+            //When it’s set to false, we attach the AsNoTracking method to
+            //our query to inform EF Core that it doesn’t need to track changes
+            //for the required entities. This greatly improves the speed of a query.
+                   
+            _repositoryContext.Set<T>().AsNoTracking() :
+            _repositoryContext.Set<T>();
+        //RepositoryContext.Set<T>().AsNoTracking() :
+        //RepositoryContext.Set<T>();
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> 
+            expression, bool trackChanges) =>
+        !trackChanges ? 
+            _repositoryContext.Set<T>().Where(expression).AsNoTracking() :
+            _repositoryContext.Set<T>().Where(expression);
+
+        public void Create(T entity)=>_repositoryContext.Set<T>().Add(entity);
+
+        public void Update(T entity)=> _repositoryContext.Set<T>().Update(entity);
+
+        public void Delete(T entity) => _repositoryContext.Set<T>().Remove(entity);
+
+    }
+}
