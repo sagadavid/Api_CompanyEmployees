9171974a55ee6dbe42b2daa99daaf3a29df7339f
sagadavid <epostsaga@gmail.com>
2023-03-17 11:49:33 +0100

    validation in positional records

--- Shared/DataTransferObjects/EmployeeForCreationDto.cs
+++ Shared/DataTransferObjects/EmployeeForCreationDto.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shared.DataTransferObjects
 {
-    public record EmployeeForCreationDto(string Name, int Age, string Position);
+    public record EmployeeForCreationDto
+        (
+        //validation in positional record
+        //validation while creation.. 'cause dto is deserializing request body.. bring validation here !
+        [Required(ErrorMessage = "Employee name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters." )]
+        string Name,
+        
+        [Required(ErrorMessage = "Age is a required field.")]
+        int Age,
+        
+        [Required(ErrorMessage = "Position is a required field.")]
+        [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20 characters.")]
+        string Position
+        );
 }
