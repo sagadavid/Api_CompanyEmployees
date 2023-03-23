a2f7f7d5ae1f013a176f4ef36187a34fae69e5a3
sagadavid <epostsaga@gmail.com>
2023-03-23 11:07:49 +0100

    inbuilt rest tool implemented with searchterm and agefilter

--- Repository/Extensions/EmployeesRepoExtensions.cs
+++ Repository/Extensions/EmployeesRepoExtensions.cs
@@ -12,16 +12,24 @@ namespace Repository.Extensions
         public static IQueryable<Employee> FilterEmployees
             (this IQueryable<Employee> employees, uint minAge, uint maxAge) =>
             employees.Where(e => (e.Age >= minAge && e.Age <= maxAge));
+        /*
+* https://localhost:7165/api/companies/3d490a70-94ce-4d15-9494-5248280c2ce3/employees?pageNumber=2&pageSize=2&searchTerm=m
+         headers/xpagination:
+        {"CurrentPage":2,"TotalPages":4,"PageSize":2,"TotalCount":7,"HasPrevious":true,"HasNext":true}
+         */
 
         public static IQueryable<Employee> SearchTerm
             (this IQueryable<Employee> employees, string searchTerm)
         {
             if (string.IsNullOrWhiteSpace(searchTerm)) return employees;
-
             var lowerCaseTerm = searchTerm.Trim().ToLower();
-
             return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
+            
+/* 
+ * https://localhost:7165/api/companies/3d490a70-94ce-4d15-9494-5248280c2ce3/employees?searchTerm=ae 
+ */
+            
         }
-        
+
     }
 }
